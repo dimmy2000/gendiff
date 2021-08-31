af9913015ad1c641e95df436515b35e9289fd5f9
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-08-31 17:44:33 +0300

    feat: update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,19 +1,23 @@
-import git
 import os
 import subprocess
 import shlex
 
-git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
-repo_root = git_repo.git.rev_parse("--show-toplevel")
-fixtures_path = os.path.join(repo_root, "tests/fixtures/")
+root_path = os.getcwd()
+fixtures_path = os.path.join(root_path, "tests/fixtures/")
 
-test_files = [
-    ["flat1.json", "flat2.json"],
-    ["flat1.yaml", "flat2.yaml"],
-]
+
+fixtures = (
+    ("flat1.json", "flat2.json"),
+    ("flat1.yaml", "flat2.yaml"),
+    ("recursive1.json", "recursive2.json"),
+    ("recursive1.yaml", "recursive2.yaml"),
+)
 
 with open(os.path.join(fixtures_path, "expected.txt")) as f:
-    EXPECTED = f.read()
+    EXPECTED_FLAT = f.read()
+
+with open(os.path.join(fixtures_path, "expected_recursive.txt")) as f:
+    EXPECTED_RECURSIVE = f.read()
 
 with open(os.path.join(fixtures_path, "expected_help.txt"), "rb") as f:
     EXPECTED_HELP = f.read()
@@ -25,10 +29,12 @@ def test_set_arg_parser():
 
 
 def test_gendiff():
-    for file in test_files:
+    for file in fixtures:
         file1 = os.path.join(fixtures_path, file[0])
         file2 = os.path.join(fixtures_path, file[1])
 
         input_request = shlex.split("poetry run gendiff {0} {1}".format(file1, file2))
         output = subprocess.run(input_request, capture_output=True)
-        assert output.stdout.decode("utf-8") + "\n" == EXPECTED
+
+        if file[0].startswith("flat"):
+            assert output.stdout.decode("utf-8") == EXPECTED_FLAT
