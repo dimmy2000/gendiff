b9a911a54f2a61b45ca1487818e2ae43abd31845
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-09-07 13:43:54 +0300

    fix: refactor fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,20 +5,15 @@ import shlex
 root_path = os.getcwd()
 fixtures_path = os.path.join(root_path, "tests/fixtures/")
 
-
-flat_fixtures = (
-    ("flat1.json", "flat2.json"),
-    ("flat1.yaml", "flat2.yaml"),
-    ("flat1.json", "flat2.yaml"),
-    ("flat1.yaml", "flat2.json"),
+fixtures = (
+    ("file1.json", "file2.json"),
+    ("file1.yaml", "file2.yaml"),
+    ("file1.json", "file2.yaml"),
+    ("file1.yaml", "file2.json"),
 )
 
-recursive_fixtures = (
-    ("recursive1.json", "recursive2.json"),
-    ("recursive1.yaml", "recursive2.yaml"),
-    ("recursive1.yaml", "recursive2.json"),
-    ("recursive1.json", "recursive2.yaml"),
-)
+flat_fixtures = os.path.join(fixtures_path, "flat")
+recursive_fixtures = os.path.join(fixtures_path, "recursive")
 
 with open(os.path.join(fixtures_path, "expected.txt")) as f:
     EXPECTED_FLAT = f.read()
@@ -36,9 +31,9 @@ def test_set_arg_parser():
 
 
 def test_flat_files():
-    for file in flat_fixtures:
-        file1 = os.path.join(fixtures_path, file[0])
-        file2 = os.path.join(fixtures_path, file[1])
+    for file in fixtures:
+        file1 = os.path.join(flat_fixtures, file[0])
+        file2 = os.path.join(flat_fixtures, file[1])
 
         input_request = shlex.split("poetry run gendiff {0} {1}".format(file1, file2))
         output = subprocess.run(input_request, capture_output=True)
@@ -47,9 +42,9 @@ def test_flat_files():
 
 
 def test_recursive_files():
-    for file in recursive_fixtures:
-        file1 = os.path.join(fixtures_path, file[0])
-        file2 = os.path.join(fixtures_path, file[1])
+    for file in fixtures:
+        file1 = os.path.join(recursive_fixtures, file[0])
+        file2 = os.path.join(recursive_fixtures, file[1])
 
         input_request = shlex.split("poetry run gendiff {0} {1}".format(file1, file2))
         output = subprocess.run(input_request, capture_output=True)
